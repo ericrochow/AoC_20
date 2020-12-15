c8a36dbcbab7c536ff46368966e7d6549a4e75b7
Eric Rochow <erochow@liquidweb.com>
2020-12-14 19:46:18 -0500

    days 13 and 14

--- solutions/day13.py
+++ solutions/day13.py
@@ -2,16 +2,70 @@
 
 from utils import read_input
 
+TEST_INPUT = ["939", "7,13,x,x,59,x,31,19"]
 
-def part_one():
-    pass
+TEST_INPUT_2 = ["100", "67,7,59,61"]
+
+TEST_INPUT_3 = ["100", "67,x,7,59,61"]
+
+TEST_INPUT_4 = ["100", "1789,37,47,1889"]
 
 
-def part_two():
-    pass
+def enumerate_busses(bus_list: list) -> list:
+    """"""
+    en_list = list(enumerate(bus_list))
+    while True:
+        try:
+            en_list.sort(key=lambda x: int(x[1]), reverse=True)
+            print("Generated list")
+            return en_list
+        except ValueError:
+            for idx, x in enumerate(en_list):
+                if x[1] == "x":
+                    del en_list[idx]
+
+
+def part_one(earliest, bus_list):
+    soonest_runs = {}
+    busses = [int(bus) for bus in bus_list if bus != "x"]
+    for bus in busses:
+        bus_start = bus
+        while bus_start < earliest:
+            bus_start += bus
+        soonest_runs[bus] = bus_start
+    soonest_depart = min(soonest_runs.values())
+    bus_id = [key for key in soonest_runs if soonest_runs[key] == soonest_depart][0]
+    minutes_to_wait = soonest_depart - earliest
+    return bus_id * minutes_to_wait
+
+
+def part_two(bus_list):
+    # TODO: Generate lists of departure times less than arbitrary max
+    # Perform unions of bus`index` start times + index
+    arbitrary_min = 100000000000000
+    arbitrary_max = 1000000000000000
+    # start_list = {x for x in range(arbitrary_min, arbitrary_max)}
+    start_list = set()
+    bus_list = enumerate_busses(bus_list)
+    # for index, bus in enumerate(bus_list):
+    for index, bus in bus_list:
+        if bus != "x":
+            if start_list:
+                bus_starts = {x for x in start_list if (x + index) % int(bus) == 0}
+                start_list.intersection_update(bus_starts)
+            else:
+                start_list = {
+                    x
+                    for x in range(arbitrary_min, arbitrary_max)
+                    if (x + index) % int(bus) == 0
+                }
+    return start_list.pop()
 
 
 if __name__ == "__main__":
     INPUT = read_input(13)
-    print(part_one())
-    print(part_two())
+    # INPUT = TEST_INPUT_2
+    EARLIEST = int(INPUT[0])
+    BUS_LIST = [x for x in INPUT[1].split(",")]
+    print(part_one(EARLIEST, BUS_LIST))
+    print(part_two(BUS_LIST))
--- solutions/day14.py
+++ solutions/day14.py
@@ -1,17 +1,122 @@
 #!/usr/bin/env python3
 
+from collections import deque
+from itertools import product
+import re
+
 from utils import read_input
 
+TEST_INPUT = [
+    "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X",
+    "mem[8] = 11",
+    "mem[7] = 101",
+    "mem[8] = 0",
+]
 
-def part_one():
-    pass
+TEST_INPUT_2 = [
+    "mask = 000000000000000000000000000000X1001X",
+    "mem[42] = 100",
+    "mask = 00000000000000000000000000000000X0XX",
+    "mem[26] = 1",
+]
 
 
-def part_two():
-    pass
+def int_str_to_binary_str(value: str) -> str:
+    """"""
+    binary_string = bin(int(value)).lstrip("0b")
+    integer_string = int(binary_string)
+    return str(integer_string)
+
+
+def bit_string_to_int(bit_list: list) -> int:
+    """
+    Convert a list of bits to an integer.
+
+    Args:
+      bit_list: A list of bits representing a binary number
+    Returns:
+      An integer specifying the base 10 representation of the bit list.
+    """
+    bit_list.reverse()
+    output = 0
+    for idx, bit in enumerate(bit_list):
+        output += bit * 2 ** idx
+    return output
+
+
+def apply_bitmask_v1(value: str, mask: str) -> list:
+    """"""
+    value = value.zfill(36)
+    value, mask = list(value), list(mask)
+    for idx, val in enumerate(mask):
+        if val != "X":
+            value[idx] = val
+    for idx, val in enumerate(value):
+        value[idx] = int(val)
+    return value
+
+
+def apply_bitmask_v2(value: str, mask: str) -> list:
+    value = value.zfill(36)
+    value, mask = list(value), list(mask)
+    for idx, val in enumerate(mask):
+        if val != "0":
+            # TODO: 1's aren't getting copied for some reason
+            # print(f"Changing {value[idx]} to {val}")
+            value[idx] = val
+    return value
+
+
+def find_floating_values(
+    mask: list,
+) -> list:
+    floaters = mask.count("X")
+    # print(mask)
+    possible = deque(map(deque, product(range(2), repeat=floaters)))
+    possibles = []
+    while possible:
+        p = possible.popleft()
+        page = mask.copy()
+        for i, m in enumerate(page):
+            if m == "X":
+                page[i] = str(p.pop())
+        page = [int(x) for x in page]
+        possibles.append(bit_string_to_int(page))
+    return possibles
+
+
+def part_one(input_code):
+    memory = {}
+    for line in input_code:
+        if line.startswith("mask"):
+            mask = line.split()[2]
+        elif line.startswith("mem"):
+            page, val = re.findall(r"\d+", line)
+            val = int_str_to_binary_str(val)
+            val = apply_bitmask_v1(val, mask)
+            val = bit_string_to_int(val)
+            memory[page] = val
+    return sum(memory.values())
+
+
+def part_two(input_code):
+    memory = {}
+    for line in input_code:
+        if line.startswith("mask"):
+            mask = line.split()[2]
+        if line.startswith("mem"):
+            page, val = re.findall(r"\d+", line)
+            page = int_str_to_binary_str(page)
+            page_mask = apply_bitmask_v2(page, mask)
+            possibles = find_floating_values(page_mask)
+            for p in possibles:
+                memory[p] = int(val)
+    return sum(memory.values())
+    # return memory
 
 
 if __name__ == "__main__":
     INPUT = read_input(14)
-    print(part_one())
-    print(part_two())
+    # INPUT = TEST_INPUT_2
+    print(part_one(INPUT))
+    print(part_two(INPUT))
