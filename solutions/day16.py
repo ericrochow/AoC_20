966adf5e90dca0a79b8eb9046e211083707f7e1f
eric.rochow <eric.rochow@msufcu.org>
2020-12-16 17:18:10 -0500

    Solved 16p2 manually; wrote out steps in english

--- solutions/day16.py
+++ solutions/day16.py
@@ -3,9 +3,9 @@
 import ipdb  # noqa: F401
 import re
 
-from utils import read_input
+from utils import read_input, read_template
 
-TEST_INPUT = (
+TEST_INPUT = [
     "class: 1-3 or 5-7",
     "row: 6-11 or 33-44",
     "seat: 13-40 or 45-50",
@@ -18,7 +18,12 @@ TEST_INPUT = (
     "40,4,50",
     "55,2,20",
     "38,6,12",
-)
+]
+
+
+TEST_INPUT_2 = [
+    "",
+]
 
 
 def find_valid_ranges(ticket_info: list) -> list:
@@ -26,6 +31,7 @@ def find_valid_ranges(ticket_info: list) -> list:
     for line in ticket_info:
         valid_ranges += re.findall(r"\d+\-\d+", line)
     return valid_ranges
+
 
 
 def ranges_to_numbers(valid_ranges: list) -> set:
@@ -45,10 +51,13 @@ def define_nearby_tickets(ticket_info: list) -> list:
         if nearby_found:
             line = [int(num) for num in line.split(",")]
             nearby.append(line)
-            # nearby += line.split(",")
         elif "nearby" in line:
             nearby_found = True
     return nearby
+
+
+def define_my_ticket(ticket_info: list) -> list:
+    pass
 
 
 def find_invalid_numbers(valid_numbers: list, nearby_tickets: list) -> list:
@@ -61,24 +70,92 @@ def find_invalid_numbers(valid_numbers: list, nearby_tickets: list) -> list:
     ]
 
 
+def define_ticket_rules(parsed_rules: list) -> dict:
+    rules = {}
+    for rule in parsed_rules:
+        rules[rule[0]] = [rule[1], rule[2]]
+    return rules
+
+
+def remove_invalid_tickets(tickets: list, valid_numbers: set) -> list:
+    valid_tickets =[]
+    for ticket in tickets:
+        skip = False
+        for value in ticket:
+            if value not in valid_numbers:
+                skip = True
+                break
+        if not skip:
+            valid_tickets.append(ticket)
+    return valid_tickets
+
+
+def detect_rule_order_possibilities(tickets: list, rules: dict) -> dict[list]:
+    rule_order = {}
+    for col in range(0, len(tickets[0])):
+        col_vals = set()
+        for ticket in tickets:
+            col_vals.add(ticket[col])
+        possible = []
+        for name, rule in rules.items():
+            if col_vals.intersection(rule) == col_vals:
+                possible.append(name)
+        if possible:
+            rule_order[col] = possible
+    return rule_order
+
+
+def deduplicate_order_possibilities(possibilities: dict[list]) -> dict:
+    """
+    Iterates over each k, v pair until it finds one with a single value.
+    Remove that value from all k, v pairs with len(v) > 1.
+    Loop until all all v are len 1
+    Return dict
+    """
+    deduped = []
+    while len(deduped) < len(rule_order):
+        deduped = []
+        for k, v in rule_order:
+            if len(v) == 1 and v not in deduped:
+                dedup = v
+        for k, v in rule_order.items():
+            if len(v) > 1:
+            try:
+                rule_order[k].remove(dedup)
+            except ValueError:
+                pass
+        deduped.append(dedup)
+    return rule_order
+
 def part_one(ticket_info):
     valid_ranges = find_valid_ranges(ticket_info)
     nearby_tickets = define_nearby_tickets(ticket_info)
     valid_numbers = ranges_to_numbers(valid_ranges)
-    invalid_tickets = find_invalid_numbers(valid_numbers, nearby_tickets)
-    return sum(invalid_tickets)
+    invalid_numbers = find_invalid_numbers(valid_numbers, nearby_tickets)
+    return sum(invalid_numbers)
 
 
-def part_two(ticket_info):
-    valid_ranges = find_valid_ranges(ticket_info)
+def part_two(ticket_info: list, parsed_rules: list):
+    rules = define_ticket_rules(parsed_rules)
+    for rule_name, rule_values in rules.items():
+        rules[rule_name] = ranges_to_numbers(rules[rule_name])
+    valid_numbers = set.union(*rules.values())
+    # write function to extract my ticket
+    my_ticket = define_my_ticket(ticket_info)
     nearby_tickets = define_nearby_tickets(ticket_info)
-    valid_numbers = ranges_to_numbers(valid_ranges)
-    invalid_tickets = find_invalid_numbers(valid_numbers, nearby_tickets)
-    return sum(invalid_tickets)
+    valid_tickets = remove_invalid_tickets(nearby_tickets, valid_numbers)
+    possibilities = detect_rule_order_possibilities(valid_tickets, rules)
+    rule_order = deduplicate_order_possibilities(possibilities)
+    # write function to find keys of values that start with departure
+    # write function to multiply values of my ticket that match index departure rules
+    # return product(
+    ipdb.set_trace()
 
 
 if __name__ == "__main__":
+    TEMPLATE = read_template(16)
     INPUT = read_input(16)
     # INPUT = TEST_INPUT
+    PARSED_RULES= [TEMPLATE.ParseText(x) for x in INPUT].pop()
     print(part_one(INPUT))
-    print(part_two(INPUT))
+    print(part_two(INPUT, PARSED_RULES))
