b5c5fca7cdacac7fc40e890d6e47f1a0c06cdfa0
Eric Rochow <ericrochow@gmail.com>
2020-12-18 20:13:45 -0500

    day18 solved

--- solutions/day18.py
+++ solutions/day18.py
@@ -1,28 +1,13 @@
 #!/usr/bin/env python3
 
-
-import ipdb  # noqa: F401
 import re
 
 from utils import read_input
 
-TEST_INPUT = [
-    "1 + 2 * 3 + 4 * 5 + 6",
-    "1 + (2 * 3) + (4 * (5 + 6))",
-    "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))",
-    "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2",
-    "5 + (8 * 3 + 9 + 3 * 4 * 3)",
-]
-# expect ["71", "51", "12240", "13632", "437"]
 
-
-PAREN_RE = re.compile(r"\([^\(]+\)")
-OPERATOR_RE = re.compile(r"[\+\*]")
-
-
-def math_operation(expression: str) -> str:
+def math_operation_no_precedence(expression: str) -> str:
     """
-    Performs a match operation left to right regardless of normal mathmatical
+    Performs a math operation left to right regardless of normal mathmatical
         precendence of multiplication over addition. It is expected that the expression
         does not have parenthesis.
 
@@ -44,66 +29,84 @@ def math_operation(expression: str) -> str:
     return str(final)
 
 
-def extract_parens(expression: str) -> str:
+def math_operation_reverse_precedence(expression: str) -> str:
     """
-    Extracts and evaluates mathematical operations within parenthesis.
+    Performs a math operation reversing the precendence of multiplication over
+    addition by prioritising addition over multiplication. It is expected that the
+    expression does not have parenthesis.
 
     Args:
-      expression: A string continaing a mathematic operation
+      expression: A string containing a mathmatical expression.
     Returns:
-      A string containing the original expression with the parenthesis broken down.
+      A string containing the final value of the expression.
     """
-    while "(" in expression:
-        parens_deep = 0
-        for index, value in enumerate(expression.split()):
-            ipdb.set_trace()
-            if value.startswith("("):
-                expression_start = index
-                parens_deep += value.count("(")
-                ipdb.set_trace()
-            elif value.endswith(")"):
-                expression_end = index
-                parens_deep -= value.count(")")
-                ipdb.set_trace()
-                if parens_deep == 0:
-                    to_evaluate = (
-                        " ".join(expression[expression_start : expression_end + 1])
-                        .lstrip("(")
-                        .rstrip(")")
-                    )
-                    ipdb.set_trace()
-                    after = math_operation(to_evaluate)
-                    expression = (
-                        f"{' '.join(expression[0:expression_start])} {after}"
-                        f"{' '.join(expression[expression_end + 1:])}"
-                    )
-                    continue
-    ipdb.set_trace()
-    return expression
+    elements = expression.split()
+    addition_evaluated = []
+    final = 1
+    for index, value in enumerate(elements):
+        if value == "*":
+            addition_evaluated.append(value)
+        elif index == 0:
+            addition_evaluated.append(int(value))
+        elif index % 2 == 0 and index >= 2 and elements[index - 1] == "+":
+            if addition_evaluated[-1] in ["+", "*"]:
+                addition_evaluated.append(int(value))
+            else:
+                addition_evaluated[-1] += int(value)
+        elif addition_evaluated[-1] == "*":
+            addition_evaluated.append(int(value))
+    for index, value in enumerate(addition_evaluated):
+        if index == 0:
+            final *= int(value)
+        if index % 2 == 0 and index >= 2 and addition_evaluated[index - 1] == "*":
+            final *= int(value)
+    return str(final)
 
 
-def evaluate_expression(expression: str) -> str:
+def extract_parens(expression: str, part: int) -> str:
     """
-    Addition and multiplication now have the same precedence so they are evaluated
-        left-to-right.
+    Recursively resolve math operations within parenthesis until there are no more
+        parenthesis.
+
+    Args:
+      expression: The original mathematical operation to evaluate
+      part: An integer specifying the part whose instructions to follow
+    Returns:
+      A string with the simplified expression.
     """
-    pass
+    paren_re = re.compile(r"\([^\(]+?\)")
+    simplified = expression
+    while "(" in simplified:
+        parens = paren_re.findall(simplified)
+        for paren in parens:
+            expr = paren.lstrip("(").rstrip(")")
+            if part == 1:
+                solved = math_operation_no_precedence(expr)
+            elif part == 2:
+                solved = math_operation_reverse_precedence(expr)
+            simplified = simplified.replace(paren, solved)
+    return simplified
 
 
-def part_one(ops: list):
-    r = []
+def part_one(ops: list) -> int:
+    results = []
     for line in ops:
-        line = extract_parens(line)
-        r.append(math_operation(line))
-    return r
+        line = extract_parens(line, 1)
+        solved = math_operation_no_precedence(line)
+        results.append(math_operation_no_precedence(solved))
+    return sum(int(r) for r in results)
 
 
-def part_two():
-    pass
+def part_two(ops: list) -> int:
+    results = []
+    for line in ops:
+        line = extract_parens(line, 2)
+        solved = math_operation_reverse_precedence(line)
+        results.append(math_operation_reverse_precedence(solved))
+    return sum(int(r) for r in results)
 
 
 if __name__ == "__main__":
     INPUT = read_input(18)
-    INPUT = TEST_INPUT
     print(part_one(INPUT))
-    print(part_two())
+    print(part_two(INPUT))
